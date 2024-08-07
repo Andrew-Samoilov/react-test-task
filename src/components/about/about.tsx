7ec7a69917719487ddbcc1046c627ec1f7937c24
andrew samoilov <89135041+Andrew-Samoilov@users.noreply.github.com>
2024-08-07 11:35:40 +0300

    add: about section

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,9 +1,15 @@
+import './about.css'
 import space from './space-video.jpg'
+
 export default function About() {
     return (
         <section>
-            <h2>About us</h2>
-            <img src={space} alt="about us video"></img>
+            <h2 id='about'>About us</h2>
+            <img
+                src={space}
+                alt="about us video"
+                width='1321px;'
+            ></img>
         </section>
     );
 }
--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,11 +1,28 @@
 import './menu.css'
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
 import { menuItems } from './menuItems'
+import { useEffect } from 'react';
+
+const useScrollToAnchor = () => {
+    const { hash } = useLocation();
+    useEffect(() => {
+        if (hash) {
+            const anchor = document.querySelector(hash);
+            if (anchor) {
+                anchor.scrollIntoView({ behavior: 'smooth' });
+            }
+        }
+    }, [hash]);
+};
 
 export default function Menu() {
+    useScrollToAnchor();
     return (
         <ul>
-            {menuItems.map((item) => <Link to={item.url} key={item.id}>{item.name}</Link>)}
+            {menuItems.map((item) =>
+            <li key={item.id}>
+                <Link to={item.url}>{item.name}</Link>
+            </li>)}
         </ul>
     )
 }
